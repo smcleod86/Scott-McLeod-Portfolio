d7688701d5aefbf4bdf3a7aaf991595adee3c391
smcleod86 <mcleod.scott_86@yahoo.com>
2020-08-27 10:57:20 -0700

    added photos

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+export default function About() {
+    return (
+        <div>
+            <Navbar />
+            <div>
+                <h1> Hi! My name is Scott.🥔</h1>
+            </div>
+        </div>
+    )
+}
